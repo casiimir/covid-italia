8ae421931b183f97348299e082b90d7bce72b3e9
casiimir <cp.ciancimino@gmail.com>
2020-12-23 00:27:47 +0100

    Add: illustrator rights

--- src/Footer.js
+++ src/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <p>
         L'applicazione utilizza i dati giornalieri della protezione civile.
         Questo piccolo contributo è il mio ringraziamento al lavoro di chi aiuta l'intero paese a resistere contro questa grande crisi. Ai medici, alle forze armate, alla protezione civile e ad ogni singolo italiano che presta attenzione alla cura dell'altro, <strong>Grazie</strong></p>
-      <p>Realizzato con ❤️ da <a href="https://www.linkedin.com/in/casimiro-p-ciancimino/">Casimiro</a></p>
+      <p>Realizzato con ❤️ da <a href="https://www.linkedin.com/in/casimiro-p-ciancimino/">Casimiro</a> <br /><italic style={{fontSize: "0.8em"}}>Illustration by <a href="https://dribbble.com/Ivan_Haidutski">Ivan Haidutski</a> from <a href="https://icons8.com/">Icons8</a></italic></p>
     </div>
   )
 }
